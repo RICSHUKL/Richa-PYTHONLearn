0b0e4982ef10bdbb89dc08d074930edc31c50ebe
RICSHUKL <43141584+RICSHUKL@users.noreply.github.com>
2018-09-13 23:02:01 +0530

    sync back to git v1

---
+++ source/Datatype.py
@@ -0,0 +1,18 @@
+name ="Richa"
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[-4]) #to get reverse order
+print(name[-1 + 3])
+
+
+a=10
+b=11
+print(a,b)
+print(name[1], name[2])
+print(name[1],end="");print(name[2])
+print(name[1],end="%");print(name[2])
+print(name[1],end="\\");print(name[2])
---
+++ source/id_print.py
@@ -0,0 +1,20 @@
+a = b = c = 2
+print(id(a),id(b),id(c))
+
+print(a,b,c)
+
+a = b = 'richa'
+
+print(id(a),id(b))
+
+a = b = [1,2,3]
+
+print(id(a),id(b))
+
+d ="""THIS IS A\n
+TEST"""
+print(d)
+
+d ="""THIS IS A \
+TEST """
+print(d)
